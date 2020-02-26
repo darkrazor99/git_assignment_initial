984a1ce7a271181bd83dc1a34d83fa1ee1378d0a
Hussien <hussienashraf99@gmail.com>
2020-02-26 19:37:01 +0200

    Create CircleVolume.java
    Added class Circle Volue which does nothing but prints the output of zero as circle has no volume

---
+++ src/CircleVolume.java
@@ -0,0 +1,8 @@
+
+public class CircleVolume implements ISubscriber {
+	@Override
+	public void notifySubscriber(String input) {
+		// TODO Auto-generated method stub
+		System.out.println("Circle Volume for input of radius: " + input + " is 0.");
+	}
+}
