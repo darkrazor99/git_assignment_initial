5af8ceaac9984eebcf7c137d65d014313f0d53ba
Muhammad Bakr <36138708+MBakr007@users.noreply.github.com>
2020-02-27 23:30:05 +0200

    Update SphereArea.java

--- src/SphereArea.java
+++ src/SphereArea.java
@@ -4,7 +4,7 @@ public class SphereArea implements ISubscriber {
     @Override
     public void notifySubscriber(String input) {
 
-        System.out.println("Sphere volume : " + 4.0*(Math.PI)* (Math.pow(Double.parseDouble(input),2)));
+        System.out.println("Sphere area : " + 4.0*(Math.PI)* (Math.pow(Double.parseDouble(input),2)));
 
     }
 }
