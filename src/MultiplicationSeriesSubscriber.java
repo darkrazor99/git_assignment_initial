8bb55f2d903b55f5f99dfaed5583403315f8528e
tarek <36449641+darkrazor99@users.noreply.github.com>
2020-02-28 10:18:02 +0200

    implemented multiplicationseries 20170134

--- src/MultiplicationSeriesSubscriber.java
+++ src/MultiplicationSeriesSubscriber.java
@@ -1,4 +1,17 @@
 
-public class MultiplicationSeriesSubscriber {
+public class MultiplicationSeriesSubscriber implements ISubscriber {
+	@Override
+	public void notifySubscriber(String input) {
+		
+		System.out.println("MultiplicationSeries : " + getMultibS(Integer.parseInt(input)));
+		
+	}
+	public int getMultibS(int n) {
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum+= n*i;
+		}
+		return sum;
+	}
 
 }
